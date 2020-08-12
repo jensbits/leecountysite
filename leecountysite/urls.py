78649d4000c11162072af1efed60ffc9de7d17d2
jenkang <jen@epicenterconsulting.com>
2020-08-11 22:33:59 -0500

    Added views

--- leecountysite/urls.py
+++ leecountysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from publicdata import views as publicdata_views
 
 urlpatterns = [
+    path('', publicdata_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
